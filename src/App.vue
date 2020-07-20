e3f3fb1ddac802101e5e28574be5c2e4fd0e6cc7
unknown <sistemas@VRE_NB_CAP1804.inacap.cl>
2020-07-20 13:24:24 -0400

    Firts Commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app" class="container">
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <h2><span class="card-title">Tu oro es {{ oro_total }}</span></h2>
+
+    <div class="row">
+      <div class="col s6">
+        <findgold  titulo="farm" :max_gold="20" :min_gold="10" />
+      </div>
+      <div class="col s6">
+        <findgold  titulo="cave" :max_gold="10" :min_gold="5"/>
+      </div>
+      <div class="col s6">
+        <findgold  titulo="house" :max_gold="5" :min_gold="2"/>
+      </div>
+      <div class="col s6">
+        <findgold  titulo="casino" :max_gold="50" :min_gold="-50"/>
+      </div>      
+    </div>
+    <h1>Historial de actividades</h1>
+    <div class="actividad" >{{sumar_info}}
+
+    </div>
+  </div>
+</template>
+
+<script>
+import findgold from './components/findgold.vue'
+import storeOro from './storeOro.js'
+
+export default {
+  name: 'App',
+  data() {
+    return  storeOro.state
+  },
+  components: {
+    findgold
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/findgold.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card  darken-1">
+      <h3>{{ titulo }}</h3>
+      <p>Gana {{min_gold}} - {{max_gold}}</p>
+      <div class="card-content white-text"> 
+        <p><button v-on:click="generarOro" class="waves-effect waves-light btn lime darken-1">Obtener oro</button></p>
+      </div>
+  </div>
+</template>
+
+<script>
+import storeOro from '../storeOro';
+
+export default {
+  name: "findgold",
+ 
+
+  props: {
+    titulo: String,
+    max_gold: Number,
+    min_gold: Number,
+  },
+   methods: {
+    generarOro: function() {
+      let nuevoPuntaje = Math.floor(Math.random() * (this.max_gold - this.min_gold + 1) ) + this.min_gold;
+      storeOro.sumar_oro(nuevoPuntaje);
+
+      let actividad = ('Se modifico' + nuevoPuntaje +' de oro en '+ this.titulo);
+
+      storeOro.sumar_info(actividad);
+    }
+  }
+}
+</script>
