0a9e3aa4f0f7cb91ea77ba94cfda647fda71031d
unknown <sistemas@VRE_NB_CAP1804.inacap.cl>
2020-07-21 10:19:53 -0400

    Actividad con fecha

--- src/components/findgold.vue
+++ src/components/findgold.vue
@@ -22,10 +22,16 @@ export default {
   },
    methods: {
     generarOro: function() {
+      const fecha = new Date();
+      const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute:'2-digit', hour12: true};
+      const fechaActual =  fecha.toLocaleDateString("es-CL", options);
+
+
+
       let nuevoPuntaje = Math.floor(Math.random() * (this.max_gold - this.min_gold + 1) ) + this.min_gold;
       storeOro.sumar_oro(nuevoPuntaje);
 
-      let actividades = ('Se modifico ' + nuevoPuntaje +' de oro en '+ this.titulo);
+      let actividades = ('Se modifico ' + nuevoPuntaje +' de oro en '+ this.titulo + ' el ' + fechaActual);
 
       storeOro.historiales(actividades);
     }
